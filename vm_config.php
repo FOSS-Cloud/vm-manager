a56463b78079f50e78c064e2bdbbc178818121fd
Tiziano Müller <tiziano.mueller@stepping-stone.ch>
2012-12-07 09:51:08 +0100

    Bump version to 1.1.2 (instead of 1.1.1 due to bug #163).

--- vm_config.php
+++ vm_config.php
@@ -72,7 +72,7 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		'virtualization' => array(
-			'version' => '1.1.0',
+			'version' => '1.1.2',
 			// Don't change the following params if you don't know what you are doing
 			'spiceByName' => false,
 			'disableSpiceAcceleration' => false,
