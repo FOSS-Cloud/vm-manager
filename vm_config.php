e1e29c6888a89b5b37dc7dab3237f7557e3348e2
Tiziano Müller <tm@dev-zero.ch>
2013-03-01 15:47:16 +0100

    Bump version to 1.2.14

--- vm_config.php
+++ vm_config.php
@@ -72,7 +72,7 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		'virtualization' => array(
-			'version' => '1.2.13',
+			'version' => '1.2.14',
 			// Don't change the following params if you don't know what you are doing
 			'spiceByName' => false,
 			'disableSpiceAcceleration' => false,
