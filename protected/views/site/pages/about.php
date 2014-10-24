c60348fd4cc56586bc0f84bc3fc637b3e11454f6
Christian Wittkowski <wittkowski@devroom.de>
2014-10-24 11:38:56 +0200

    new donate button

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -39,7 +39,17 @@ $this->title = 'About';
 <p>The FOSS-Cloud solution is the most advanced pure Open Source Cloud in the marketplace today. </p>
 <p>Before using, the FOSS-Cloud team would like to remind you that the primary means of sustaining the development of FOSS-Cloud is via contributions by users such as yourself. FOSS-Cloud is now and will continue to be totally free of charge; however, it takes money and resources to make FOSS-Cloud available. If you are able, please consider donating to the FOSS-Cloud Project.</p>
 <div  style="float: left;">
-<p><a href="http://sourceforge.net/donate/index.php?group_id=390416"><img src="<?php echo Yii::app()->baseUrl;?>/images/project-support.jpg" width="88" height="32" border="0" alt="Support This Project" /> </a></p>
+<p><form action="https://www.paypal.com/cgi-bin/webscr" method="post" 
+target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="797946K7GB8MN">
+<input type="image" 
+src="https://www.paypalobjects.com/en_GB/i/btn/btn_donate_LG.gif" border="0" 
+name="submit" alt="PayPal – The safer, easier way to pay online.">
+<img alt="" border="0" 
+src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" 
+height="1">
+</form></p>
 <p>Thank you for using FOSS-Cloud</p>
 <p>The FOSS-Cloud Team</p>
 </div>
