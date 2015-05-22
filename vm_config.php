b4e6c3d6b23b1d51b0dfd57fe3b1e8f7fbd6a41b
Beat Stebler <beat.stebler@foss-group.ch>
2015-05-22 11:23:44 +0200

    Bump version to 1.3.0.12

--- vm_config.php
+++ vm_config.php
@@ -72,7 +72,7 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		'virtualization' => array(
-			'version' => '1.3.0.11',
+			'version' => '1.3.0.12',
 			// Don't change the following params if you don't know what you are doing
 			'spiceByName' => false,
 			'disableSpiceAcceleration' => false,
